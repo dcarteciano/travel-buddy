9a1a9e1017202aa246a28e6ece7551170ad979d4
tkcannon <taylorkcannon@gmail.com>
2021-12-03 10:56:39 -0700

    added function to get drivetime between two lat/lon

--- mapquest-test.js
+++ mapquest-test.js
@@ -1,7 +1,17 @@
-axios.get('http://www.mapquestapi.com/directions/v2/route?key=EQrA7i7TLmnP9B1ZFC6CRQgsZVFl6XGz&from=37.79867,-122.39925&to=40.475273,-111.926357')
-  .then(function (res){
-    console.log(res);
-  })
-  .catch(function (err) {
-    console.log(err);
-});
+var driveInfo = {
+  driveTime: ""
+};
+
+query("41.121264, -111.919899", "41.169600, -112.014570");
+
+function query(latlon1, latlon2) {
+  axios.get('http://www.mapquestapi.com/directions/v2/route?key=EQrA7i7TLmnP9B1ZFC6CRQgsZVFl6XGz&from=' + latlon1 + '&to=' + latlon2 + '')
+    .then(function (res) {
+      console.log("res:", res);
+      driveInfo.driveTime = res.data.route.realTime;
+      console.log(driveInfo.driveTime);
+    })
+    .catch(function (err) {
+      console.log(err);
+    })
+}

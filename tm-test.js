e1388ee229227717236d3db8002a4a7bb75e78f2
johndavis <johndavis92790@gmail.com>
2021-12-04 09:54:09 -0700

    added basic html divs and css

--- tm-test.js
+++ tm-test.js
@@ -5,3 +5,21 @@ axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationN
   .catch(function (err) {
     console.log(err);
 });
+
+// var settings = {
+//   "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=10",
+//   "method": "GET",
+//   "timeout": 0,
+//   "headers": {
+//   "api-version": "v200",
+//   "Authorization": "Basic VU5JVl81NDpnYUhaR0NxNm1MU2o=",
+//   "client": "UNIV_54",
+//   "x-api-key": "KJxQ3LIeJi9v1XVAKbzcc5R0tCYpdsID6aAGkv1R",
+//   "device-datetime": "2020-06-18T12:07:57.296Z",
+//   "territory": "US",
+//   },
+//   };
+  
+//   $.ajax(settings).done(function (response) {
+//   console.log(response);
+//   });

8fd8db6fcb3239e34973b3a78492c4617f8c46c2
tkcannon <taylorkcannon@gmail.com>
2021-12-04 21:09:17 -0700

    added mapquest request to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,3 +37,28 @@ function addListEl(title, time, info, subInfo) {
 };
 
 addListEl(title, time, info, subInfo);
+
+// from and to can either be "lat,lon" or an adress
+// testing from console
+function getMapData(from, to) {
+  axios.get('http://www.mapquestapi.com/directions/v2/route?key=EQrA7i7TLmnP9B1ZFC6CRQgsZVFl6XGz&from=' + from + '&to=' + to + '')
+    .then(function (res) {
+      console.log("mapquest:", res.data);
+      if (res.data.route.realTime > 0) {
+        if (res.data.route.realTime < 10000000) {
+          console.log("Drive time: " + res.data.route.realTime);
+          return res.data.route.realTime; // returns drivetime data in seconds based off of realtime traffic conditions
+        }
+        else {
+          console.log("realtime data unavailable", "Drive time: " + res.data.route.time);
+          return res.data.route.time; // if realtime data is unavailable, returns calculated drivetime time in seconds
+        }
+      }
+      else {
+        console.log("Could not find route between given locations");
+      }
+    })
+    .catch(function (err) {
+      console.log("could not connect to mapquestapi.com");
+    })
+}

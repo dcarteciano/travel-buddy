d5b1034730939d7f58f9ed6924952432e1a45a0c
DC <dcarteciano@gmail.com>
2021-12-04 20:53:26 -0700

    created an array of events and assigned the long/lat to variables.

--- current-location-test.js
+++ current-location-test.js
@@ -13,6 +13,29 @@ function geoFindMe() {
     status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     mapLink.textContent = `${latitude},${longitude}`;
+    //-----
+
+    
+    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=OWIi7laz1qDwxQmUKHndhZXCYa98oavA';
+    fetch(requestUrl)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data) {
+  
+      var eventArray = data._embedded.events;
+
+      for (var i = 0; i < eventArray.length; i++) {
+        var eventLong = eventArray[i]._embedded.venues[0].location.longitude;
+        var eventLat = eventArray[i]._embedded.venues[0].location.latitude;
+        
+        console.log(eventLong)
+
+      }
+
+
+  });
+
   }
 
   function error() {
@@ -25,11 +48,44 @@ function geoFindMe() {
     status.textContent = 'Locating…';
     navigator.geolocation.getCurrentPosition(success, error);
   }
+// ticket master api to get events nearby and long/lat
+  function getApi() { 
+    var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=OWIi7laz1qDwxQmUKHndhZXCYa98oavA';
+    fetch(requestUrl)
+    .then(function (response){
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+  
+      var eventArray = data._embedded.events;
+      console.log(eventArray)
+  
+  
+      for (var i = 0; i < eventArray.length; i++) {
+        var eventInfo = document.getElementById("event")
+        eventInfo.textContent += eventArray[i].name;
+      }
+      for (var i = 0; i < eventArray.length; i++) {
+        var eventLong = eventArray[i]._embedded.venues[0].location.longitude;
+        var eventLat = eventArray[i]._embedded.venues[0].location.latitude;
 
+
+      }
+
+  });
+  
+  
+  }
+  
+  getApi();
 
 }
 
+
 document.querySelector('#find-me').addEventListener('click', geoFindMe);
+
+
 
 
 
--- tm-test.js
+++ tm-test.js
@@ -1,30 +1,30 @@
-// axios.get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=OWIi7laz1qDwxQmUKHndhZXCYa98oavA')
-//   .then(function (res){
-//     console.log(res);
+
+
+// // function that displays the events from the API
+// function getApi() { 
+//   var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=OWIi7laz1qDwxQmUKHndhZXCYa98oavA';
+//   fetch(requestUrl)
+//   .then(function (response){
+//     return response.json();
 //   })
-//   .catch(function (err) {
-//     console.log(err);
+//   .then(function (data) {
+//     console.log(data);
+
+//     var eventArray = data._embedded.events;
+//     console.log(eventArray)
+
+
+//     for (var i = 0; i < eventArray.length; i++) {
+//       var eventInfo = document.getElementById("event")
+//       eventInfo.textContent += eventArray[i].name;
+//     }
+//     for (var i = 0; i < eventArray.length; i++) {
+//       var locationData = eventArray[i]._embedded.venues[0].location
+//       console.log(locationData);
+//     }
 // });
 
-// function that displays the events from the API
-function getApi() { 
-  var requestUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=OWIi7laz1qDwxQmUKHndhZXCYa98oavA';
-  fetch(requestUrl)
-  .then(function (response){
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
 
-    var eventArray = data._embedded.events;
-    console.log(eventArray)
-    for (var i = 0; i < eventArray.length; i++) {
-      var eventInfo = document.getElementById("event")
-      eventInfo.textContent += eventArray[i].name;
-    }
-});
+// }
 
-
-}
-
-getApi();
+// getApi();

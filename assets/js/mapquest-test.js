8be2ad3bd9b393d3098e0423c7c539ebaff8ec23
tkcannon <taylorkcannon@gmail.com>
2021-12-04 17:15:36 -0700

    adjusted mapData function for bad locations

--- assets/js/mapquest-test.js
+++ assets/js/mapquest-test.js
@@ -1,15 +1,18 @@
-map = document.querySelector("#map")
-
 // from and to can either be "lat,lon" or an adress
-// still needs more error handling
 // testing from console
 function getMapData(from, to) {
   axios.get('http://www.mapquestapi.com/directions/v2/route?key=EQrA7i7TLmnP9B1ZFC6CRQgsZVFl6XGz&from=' + from + '&to=' + to + '')
     .then(function (res) {
-      console.log("res:", res.data);
-      return res.data.route.realTime; // can adjust if we want more info, currently returns drivetime in seconds based on current traffic conditions 
+      console.log("mapquest:", res.data);
+      if (res.data.route.realTime > 0) {
+        return res.data.route.realTime; // can adjust if we want more info, currently returns drivetime in seconds based on current traffic conditions 
+      }
+      else{
+        console.log("Could not find route between given locations");
+        return;
+      }
     })
     .catch(function (err) {
-      console.log(err);
+      console.log("could not connect to mapquestapi.com");
     })
 }

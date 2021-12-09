3eccadadeee94bb1a1d4e79443ca590c7e16ccdb
johndavis <johndavis92790@gmail.com>
2021-12-08 22:27:44 -0700

    added get directions button and image from event object and test code for movie showtimes

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,6 @@ function getApi(cat, currentLatitude, curentLongitude) {
 
   axios.get(requestUrl)
     .then(function (res) {
-      console.log('response', res);
       var eventArray = res.data._embedded.events;
       console.log('eventArray', eventArray);
       buildList(eventArray);
@@ -56,28 +55,30 @@ function buildList(eventArray) {
     var eventTitle = eventArray[i].name;
     var eventTime = eventArray[i].dates.start.dateTime;
     var eventInfo = eventArray[i]._embedded.venues[0].name;
-    var eventSubInfo = eventArray[i].url;
+    var eventURL = eventArray[i].url;
+    var eventSubInfo = eventArray[i].pleaseNote;
     var eventLat = eventArray[i]._embedded.venues[0].location.latitude;
     var eventLong = eventArray[i]._embedded.venues[0].location.longitude;
+    var eventPhoto = eventArray[i].images[0].url;
     getMapData(currentLatitude + ',' + curentLongitude, eventLat + ',' + eventLong,
-      eventTitle, eventTime, eventInfo, eventSubInfo);
+      eventTitle, eventTime, eventInfo, eventSubInfo, eventURL, eventPhoto);
   }
 }
 
 // from and to can either be "lat,lon" or an adress
-function getMapData(from, to, eventTitle, eventTime, eventInfo, eventSubInfo) {
+function getMapData(from, to, eventTitle, eventTime, eventInfo, eventSubInfo, eventURL, eventPhoto) {
   axios.get('https://www.mapquestapi.com/directions/v2/route?key=diSZVTUqXE3YRm5IRyRe5IWmMHZWbypB&from=' + from + '&to=' + to + '')
     .then(function (res) {
       if (res.data.route.realTime > 0) {
         if (res.data.route.realTime < 10000000) {
           var leaveByTime = moment(eventTime).subtract(res.data.route.realTime, 'seconds');
           // returns drivetime data in seconds based off of realtime traffic conditions
-          addListEl(eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime);
+          addListEl(from, to, eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime, eventURL, eventPhoto);
         }
         else {
           var leaveByTime = moment(eventTime).subtract(res.data.route.realTime, 'seconds');
           // if realtime data is unavailable, returns calculated drivetime time in seconds
-          addListEl(eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime);
+          addListEl(from, to, eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime, eventURL, eventPhoto);
         }
       }
       else {
@@ -90,10 +91,10 @@ function getMapData(from, to, eventTitle, eventTime, eventInfo, eventSubInfo) {
     })
 }
 
-function addListEl(eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime) {
+function addListEl(from, to, eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime, eventURL, eventPhoto) {
 
-  eventTime = moment(eventTime).format('MMMM Do YYYY, h:mm:ss a');
-  leaveByTime = moment(leaveByTime).format('LT');
+  eventTime = moment(eventTime).format('MMMM Do YYYY, h:mma');
+  leaveByTime = moment(leaveByTime).format('h:mma');
 
   // create a container for event
   var listEl = $('<div>');
@@ -106,25 +107,44 @@ function addListEl(eventTitle, eventTime, eventInfo, eventSubInfo, leaveByTime) 
   listFirstColBox.addClass('box').attr('style', 'width: 200px;').appendTo(listFirstColEl);
   var listEventTitle = $('<p>');
   listEventTitle.addClass('title is-4').text(eventTitle).appendTo(listFirstColBox);
-  var listEventInfo = $('<p>');
-  listEventInfo.addClass('title is-5').text(eventInfo).appendTo(listFirstColBox);
   var listEventTime = $('<p>');
-  listEventTime.addClass('title is-6').text(eventTime).appendTo(listFirstColBox);
+  listEventTime.addClass('title is-6 has-text-right').text(eventTime).appendTo(listFirstColBox);
+  var listEventDriveTime = $('<p>');
+  listEventDriveTime.addClass('title is-6 has-text-right').text('Leave by ' + leaveByTime).appendTo(listFirstColBox);
+
   var listSecColEl = $('<div>');
   listSecColEl.addClass('column').appendTo(listColEl);
   var listSecColBox = $('<div>');
   listSecColBox.addClass('box').appendTo(listSecColEl);
-  var listEventDriveTime = $('<p>');
-  listEventDriveTime.addClass('subtitle').text('Leave by ' + leaveByTime).appendTo(listSecColBox);
-
-  // https://google.com/maps/dir//Vivint+Arena/@40.6219482,-112.0623527,11
+  var listEventInfo = $('<p>');
+  listEventInfo.addClass('title is-5').text(eventInfo).appendTo(listSecColBox);
+  var listEventNote = $('<p>');
+  listEventNote.addClass('is-size-6').text(eventSubInfo).appendTo(listSecColBox);
+  var listEventNoteSpace = $('<br>');
+  listEventNoteSpace.addClass('is-size-6').appendTo(listSecColBox);
 
   var listEventSubInfo = $('<a>')
-    .attr('href', eventSubInfo)
+    .attr('href', eventURL)
     .attr('target', '_blank')
-    .addClass('button is-success')
+    .addClass('button is-success mx-3')
     .text('Get Tickets');
   listEventSubInfo.appendTo(listSecColBox);
+
+  var titleFixed = eventInfo.split(' ').join('+');
+  // https://www.google.com/maps/dir/40.4752752,-111.9263536/The+Depot/@40.6257634,-112.0496547,11
+  var listEventDirections = $('<a>')
+    .attr('href', 'https://www.google.com/maps/dir/' + from + '/' + titleFixed)
+    .attr('target', '_blank')
+    .addClass('button is-success mx-3')
+    .text('Get Directions');
+  listEventDirections.appendTo(listSecColBox);
+
+  var listEventPhotoFigure = $('<figure>');
+  listEventPhotoFigure.addClass('image is-16by9');
+  var listEventPhoto = $('<img>');
+  listEventPhoto.attr('src', eventPhoto);
+  listEventPhoto.appendTo(listEventPhotoFigure);
+  listEventPhotoFigure.appendTo(listSecColBox);
 
   listEl.appendTo(events);
 };

5accd567e16a77ef1febc61cc29cd691be3bafb6
tkcannon <taylorkcannon@gmail.com>
2021-12-11 06:01:54 -0700

    _

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function getFilms() {
       "Authorization": "Basic VU5JVl81NDpnYUhaR0NxNm1MU2o=",
       "client": "UNIV_54",
       "x-api-key": "KJxQ3LIeJi9v1XVAKbzcc5R0tCYpdsID6aAGkv1R",
-      "device-datetime": "2021-12-09T19:51:20+0000",
+      "device-datetime": "2021-12-11T06:00:20+0000",
       "territory": "US",
       },
     };
@@ -113,7 +113,7 @@ function getCurrentPos(filmID) {
 // filmGlu api to get showtimes for selected film nearby and long/lat
 function getApi(currentLatitude, currentLongitude) {
   var filmShowtimes = {
-    "url": "https://api-gate2.movieglu.com/filmShowTimes/?film_id=315323&date=2021-12-09&n=25",
+    "url": "https://api-gate2.movieglu.com/filmShowTimes/?film_id=315323&date=2021-12-11&n=25",
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -122,7 +122,7 @@ function getApi(currentLatitude, currentLongitude) {
       "Authorization": "Basic VU5JVl81NDpnYUhaR0NxNm1MU2o=",
       "client": "UNIV_54",
       "x-api-key": "KJxQ3LIeJi9v1XVAKbzcc5R0tCYpdsID6aAGkv1R",
-      "device-datetime": "2021-12-09T19:51:20+0000",
+      "device-datetime": "2021-12-11T6:00:20+0000",
       "territory": "US",
       },
     };
